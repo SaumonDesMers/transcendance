e63d61eb0281ab26729d3a88d90afc28cb1131ce
Simon Gaubert <sim.gaubert.sg@gmail.com>
2022-10-09 19:53:35 +0200

    just testing for now

---
+++ backend/backend-src/src/user/user.service.ts
@@ -0,0 +1,56 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { User } from "./user.model";
+
+@Injectable()
+export class UserService {
+	users: User[] = [];
+
+	addUser(name: string, email: string, password: string) {
+		const userId = Math.random().toString();
+		const newUser = new User(userId, name, email, password);
+		this.users.push(newUser);
+		return newUser;
+	}
+
+	getUsers(): User[] {
+		return [...this.users];
+	}
+
+	getUser(userId: string): User {
+		const index = this.findUserIndex(userId);
+		return this.users[index];
+	}
+
+	updateUser(
+		userId: string,
+		name: string,
+		email: string,
+		password: string
+	): User {
+		const index = this.findUserIndex(userId);
+		const user = this.users[index];
+		if (name) {
+			user.name = name;
+		}
+		if (email) {
+			user.email = email;
+		}
+		if (password) {
+			user.password = password;
+		}
+		return user;
+	}
+
+	deleteUser(userId: string) {
+		const index = this.findUserIndex(userId);
+		this.users.splice(index, 1);
+	}
+
+	private findUserIndex(userId: string): number {
+		const index = this.users.findIndex(user => user.id === userId);
+		if (index === -1) {
+			throw new NotFoundException("User not found");
+		}
+		return index;
+	}
+}
