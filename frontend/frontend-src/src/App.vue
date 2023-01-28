d2682ba2a461bfb47b65f30a616717c1f3d900bd
Simon Gaubert <sim.gaubert.sg@gmail.com>
2023-01-29 00:30:22 +0100

    add frontend

---
+++ frontend/frontend-src/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+	data() {
+		return {
+			count: 1
+		}
+	}
+}
+</script>
+
+<template>
+
+	<h1>Hello world !</h1>
+	
+	<button @click="count++">Count is {{count}}</button>
+
+</template>
+
+<style scoped>
+</style>
