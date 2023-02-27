3df45a989d5c50a4044b808241466eabadd4f39e
afaure <afaure@student.42.fr>
2023-02-27 14:01:20 +0100

    created new init migration from prisma

--- backend/backend-src/src/database/migrations/20230227130101_init/migration.sql
+++ backend/backend-src/src/database/migrations/20230227130101_init/migration.sql
@@ -1,9 +1,24 @@
 -- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL,
+    "username" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "userId" INTEGER NOT NULL,
+    "username" TEXT NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("userId")
+);
+
+-- CreateTable
 CREATE TABLE "ChatUser" (
-    "id" SERIAL NOT NULL,
     "userId" INTEGER NOT NULL,
 
-    CONSTRAINT "ChatUser_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "ChatUser_pkey" PRIMARY KEY ("userId")
 );
 
 -- CreateTable
@@ -29,7 +44,7 @@ CREATE TABLE "DMChannel" (
 );
 
 -- CreateTable
-CREATE TABLE "TextChannel" (
+CREATE TABLE "GroupChannel" (
     "channelId" INTEGER NOT NULL
 );
 
@@ -40,13 +55,13 @@ CREATE TABLE "_ChannelToChatUser" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "ChatUser_userId_key" ON "ChatUser"("userId");
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "DMChannel_channelId_key" ON "DMChannel"("channelId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "TextChannel_channelId_key" ON "TextChannel"("channelId");
+CREATE UNIQUE INDEX "GroupChannel_channelId_key" ON "GroupChannel"("channelId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "_ChannelToChatUser_AB_unique" ON "_ChannelToChatUser"("A", "B");
@@ -55,10 +70,13 @@ CREATE UNIQUE INDEX "_ChannelToChatUser_AB_unique" ON "_ChannelToChatUser"("A", 
 CREATE INDEX "_ChannelToChatUser_B_index" ON "_ChannelToChatUser"("B");
 
 -- AddForeignKey
-ALTER TABLE "ChatUser" ADD CONSTRAINT "ChatUser_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Message" ADD CONSTRAINT "Message_chatUserId_fkey" FOREIGN KEY ("chatUserId") REFERENCES "ChatUser"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "ChatUser" ADD CONSTRAINT "ChatUser_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_chatUserId_fkey" FOREIGN KEY ("chatUserId") REFERENCES "ChatUser"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Message" ADD CONSTRAINT "Message_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -67,10 +85,10 @@ ALTER TABLE "Message" ADD CONSTRAINT "Message_channelId_fkey" FOREIGN KEY ("chan
 ALTER TABLE "DMChannel" ADD CONSTRAINT "DMChannel_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "TextChannel" ADD CONSTRAINT "TextChannel_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "GroupChannel" ADD CONSTRAINT "GroupChannel_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_ChannelToChatUser" ADD CONSTRAINT "_ChannelToChatUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Channel"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "_ChannelToChatUser" ADD CONSTRAINT "_ChannelToChatUser_B_fkey" FOREIGN KEY ("B") REFERENCES "ChatUser"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "_ChannelToChatUser" ADD CONSTRAINT "_ChannelToChatUser_B_fkey" FOREIGN KEY ("B") REFERENCES "ChatUser"("userId") ON DELETE CASCADE ON UPDATE CASCADE;
