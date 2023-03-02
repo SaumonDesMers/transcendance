c901827e61661837b8133c9f9c8b34d180062629
afaure <afaure@student.42.fr>
2023-03-02 12:06:19 +0100

    chat service wip, sendMessage method done

--- backend/backend-src/src/chat/Chat.service.ts
+++ backend/backend-src/src/chat/Chat.service.ts
@@ -2,22 +2,52 @@ import { Injectable } from "@nestjs/common";
 import { Prisma, Channel, ChatUser, Message } from "@prisma/client";
 import { MessageRepository } from "./Message.repository";
 import { ChannelRepository } from "./Channel.repository";
+import { MessageWithAll, MessageWithAuthor, MessageWithChannel } from "./Chat.module";
 
 @Injectable()
 export class ChatService {
 	constructor(private channelRepository: ChannelRepository,
 				private messageRepository: MessageRepository) {}
 
-	async createGroupChannel()
-	{
-		
+	async createGroupChannel()	{	
 	}
 
-	async createDMChannel()
+	async createDMChannel() {
 
-	async sendMessage()
+	}
 
-	async eraseMessage()
+	async sendMessage(params: {
+		author: ChatUser;
+		channel: Channel;
+		content: string;}) {
+		const {author, channel, content} = params;
+		const message = await this.messageRepository.createMessage({
+			content: content,
+			channel: {
+				connect: {id: channel.id}},
+			author: {
+				connect: {userId: author.userId}},
+			},
+			{channel: true, author: true},
+		);
+
+		return message;
+	}
+
+	async eraseMessage(params: {
+		id: Message['id'];
+	}) {
+		const {id} = params;
+		const message = await this.messageRepository.deleteMessage({
+			where: {id}
+		},
+		{channel: true, author: {
+			select: {
+				user: {select: { username: true}}
+			}
+		}}
+		)
+	}
 
 	async joinChannel()
 
