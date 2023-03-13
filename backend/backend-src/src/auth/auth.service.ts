114d3785deee1b4fb58ec2be90cf705ca584519a
Simon Gaubert <sim.gaubert.sg@gmail.com>
2023-03-13 17:37:12 +0100

    [SIMON]: remove username from jwt payload

--- backend/backend-src/src/auth/auth.service.ts
+++ backend/backend-src/src/auth/auth.service.ts
@@ -12,22 +12,12 @@ export class AuthService {
 	) {}
 
 	async generateJWT(user: any): Promise<string> {
-		const payload = { username: user.username, sub: user.id };
+		const payload = { id: user.id };
 		return this.jwtService.sign(payload);
 	}
 
 	async verifyJWT(jwt: string): Promise<any> {
 		return this.jwtService.verify(jwt);
-	}
-
-	async findOrCreateUser(userDto: CreateUserDto): Promise<any> {
-		let user: any = null;
-		try {
-			user = await this.userService.getOneUser(userDto.id);
-		} catch {
-			user = this.userService.createUser(userDto);
-		}
-		return user;
 	}
 
 	async getUser(userId: number): Promise<any> {
