f60d9cf75b31c7d7334ae92a8e7530c4aa878065
Astrid GAULTIER <asgaulti@e1r12p6.clusters.42paris.fr>
2023-05-08 15:53:00 +0200

    mute 1

---
+++ frontend/frontend-src/src/components/mute.vue
@@ -0,0 +1,11 @@
+<!-- creer un component pour mute 
+affichage du user qui va se faire mute 
++ avec menu deroulant ou autre pour entrer un input de duree ou de date
++ cancel / save  -->
+
+
+<script>
+import chat from '../scripts/chat';
+import { CreateGroupChannelDto } from '../../../../backend/backend-src/src/chat/GroupChannel.create.dto';
+import { State } from '../scripts/state';
+import user from '../scripts/user';
